bf4c7bfa7f675a310d5ec56ef77c4a75bf28aa40
Owiti-Charles <mikeycharlesm7@gmail.com>
2020-09-02 14:03:08 +0300

    fix submit form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState(['Go home','From there']);
+  const [todos, setTodos] = useState(["Go home", "From there"]);
+  const [input, setInput] = useState("");
+  const addTodo = (event) => {
+    setTodos([...todos, input]);
+  };
   return (
     <div className="App">
-
       <h2>home</h2>
-      <input/>
-      <button>Add To-Do</button>
+      <form>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button type="submit" onClick={addTodo}>Add To-Do</button>
+      </form>
+
       <ul>
-        {todos.map(todo =>(
-          <li>{todo}</li>))}
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
       </ul>
     </div>
   );
