8f4bc1af30b96246492bbaef183ee7197be5fdbb
Owiti-Charles <mikeycharlesm7@gmail.com>
2020-09-04 08:42:32 +0300

    Add edit todo button

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ function App() {
   };
   return (
     <div className="App">
-      <h2>home</h2>
       <form>
         <FormControl>
           <InputLabel>Write a new To Do item</InputLabel>
--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   List,
   ListItem,
@@ -9,28 +9,54 @@ import {
 } from "@material-ui/core";
 import "./Todo.css";
 import db from "./firebase";
-import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteIcon from "@material-ui/icons/Delete";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Todo(props) {
-  console.log(props);
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
   return (
-    <List className="todo_list">
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar></Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary={props.todo.todo.todo}
-          secondary={props.todo.id}
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div className={classes.paper}>
+          <h2>trial Modal</h2>
+          <button onClick={(e) => setOpen(false)}>close</button>
+        </div>
+      </Modal>
+      <List className="todo_list">
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar></Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary={props.todo.todo.todo}
+            secondary={props.todo.id}
+          />
+        </ListItem>
+        <button onClick={(e) => setOpen(true)}>edit</button>
+        <DeleteIcon
+          variant="contained"
+          color="secondary"
+          type="submit"
+          onClick={(event) =>
+            db.collection("tasks").doc(props.todo.id).delete()
+          }
         />
-      </ListItem>
-      <DeleteIcon
-        variant="contained"
-        color="secondary"
-        type="submit"
-        onClick={(event) => db.collection("tasks").doc(props.todo.id).delete()}
-      />
-    </List>
+      </List>
+    </>
   );
 }
 
